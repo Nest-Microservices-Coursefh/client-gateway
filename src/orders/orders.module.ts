104b009f172a78c1d5f1b78f74b99ff0b1f6d2ad
DiegoGutierrezP <diego.sk75.dgp@gmail.com>
2025-01-19 12:46:15 -0500

    first commit

---
+++ src/orders/orders.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { envs, ORDER_SERVICE } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
+
+@Module({
+  controllers: [OrdersController],
+  providers: [],
+  imports: [
+    NatsModule,
+    // ClientsModule.register([
+    //   {
+    //     name: ORDER_SERVICE,
+    //     transport: Transport.TCP,
+    //     options: {
+    //       host: envs.orderMicroserviceHost,
+    //       port: envs.orderMicroservicePort,
+    //     },
+    //   },
+    // ]),
+  ],
+})
+export class OrdersModule {}
---
+++ src/products/products.controller.ts
@@ -0,0 +1,81 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { NATS_SERVICE } from 'src/config';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+
+  @Post()
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.client.send({ cmd: 'create_product' }, createProductDto).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
+
+  @Get()
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.client.send({ cmd: 'find_all_products' }, paginationDto);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    // try {
+    //   const product = await firstValueFrom(
+    //     this.client.send({ cmd: 'find_one_product' }, { id }),
+    //   );
+
+    //   return product;
+    // } catch (error) {
+    //   console.log(error);
+    //   throw new RpcException(error);
+    // }
+
+    return this.client.send({ cmd: 'find_one_product' }, { id }).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.client
+      .send({ cmd: 'update_product' }, { ...updateProductDto, id })
+      .pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: number) {
+    return this.client.send({ cmd: 'delete_product' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
+}
